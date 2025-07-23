ed388bffa1955ec358ebdb702df49111636cca80
ender <70382694+EnderDev7379@users.noreply.github.com>
2025-07-23 09:48:47 +0200

    Update to 1.21.5

---
+++ src/main/java/net/gooseman/dwarvendepths/item/ModItems.java
@@ -0,0 +1,21 @@
+package net.gooseman.dwarvendepths.item;
+
+import net.gooseman.dwarvendepths.DwarvenDepths;
+import net.minecraft.world.item.Item;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.neoforge.registries.DeferredItem;
+import net.neoforged.neoforge.registries.DeferredRegister;
+
+public class ModItems {
+    public static final DeferredRegister.Items ITEMS = DeferredRegister.createItems(DwarvenDepths.MODID);
+
+    public static final DeferredItem<Item> RAW_IMPURE_MITHRIL = ITEMS.registerItem("raw_impure_mithril", Item::new, new Item.Properties());
+    public static final DeferredItem<Item> RAW_MITHRIL = ITEMS.registerItem("raw_mithril", Item::new, new Item.Properties().fireResistant());
+
+    public static final DeferredItem<Item> IMPURE_MITHRIL = ITEMS.registerItem("impure_mithril", Item::new, new Item.Properties());
+    public static final DeferredItem<Item> MITHRIL = ITEMS.registerItem("mithril", Item::new, new Item.Properties().fireResistant());
+
+    public static void register(IEventBus eventBus) {
+        ITEMS.register(eventBus);
+    }
+}
